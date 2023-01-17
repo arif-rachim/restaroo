bd6038632e80e96d65af36599c9edde3061dc5e2
Arif Rachim <a.arif.r@gmail.com>
2023-01-17 21:30:49 +0400

    Upgrade package json and collection route

--- packages/adm/src/routes/CollectionRoute.tsx
+++ packages/adm/src/routes/CollectionRoute.tsx
@@ -1,14 +1,19 @@
 import {RouteProps} from "@restaroo/lib";
 import {useEffect} from "react";
 import {pocketBase} from "../service";
+import {schema} from "@restaroo/mdl";
+
+
 const pb = pocketBase;
 
 export function CollectionRoute(route:RouteProps){
     const collection = route.params.get('collection') ?? '';
+    console.log(schema.filter((f:any) => f.name === collection));
     useEffect(() => {
         (async() => {
             const pageResult = await pb.collection(collection).getFirstListItem('');
-            debugger;
+
+
         })();
     },[])
     return <div>
